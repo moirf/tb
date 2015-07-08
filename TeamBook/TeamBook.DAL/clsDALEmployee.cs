91188413784f47610dfca339e8dcfeb93027a1ff
rajesh2245977 <rajesh.sharmasonepat@gmail.com>
2015-07-08 12:36:07 +0530

    db connection changes

---
+++ TeamBook/TeamBook.DAL/clsDALEmployee.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamBook.DAL
+{
+    public class clsDALEmployee : IDisposable
+    {
+        public void Dispose()
+        { }
+        /// <summary>
+        /// This function used to Get the Employee Details List.
+        /// all parameters are optional
+        /// </summary>
+        /// <param name="SearchText"></param>
+        /// <param name="PageNumber"></param>
+        /// <param name="PageSize"></param>
+        /// <param name="SortBy"></param>
+        /// <returns></returns>
+        public DataTable GetEmployeeDetailsByEmployeeId(string EmployeeId)
+        {
+            DataTable dtDetailsList = new DataTable();
+            Database objDB = DatabaseFactory.CreateDatabase("TBConnectionSQL");
+            using (DbCommand objCMD = objDB.GetStoredProcCommand("GetEmployeeDetailsByEmployeeId"))
+            {
+                objDB.AddInParameter(objCMD, "@EmployeeId", System.Data.DbType.String, EmployeeId);
+                try
+                {
+                    dtDetailsList = objDB.ExecuteDataSet(objCMD).Tables[0];
+                }
+                catch (Exception ex)
+                {
+                   // TeamBook.DAL.Common.clsDALErrorHandler.WriteError(ex);
+                }
+            }
+            return dtDetailsList;
+
+        }
+    }
+}
--- TeamBook/TeamBook/Controllers/HomeController.cs
+++ TeamBook/TeamBook/Controllers/HomeController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TeamBook.DAL;
 namespace TeamBook.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            var dtList = (new clsDALEmployee().GetEmployeeDetailsByEmployeeId(User.Identity.Name.ToLower()));
             return View();
         }
 
